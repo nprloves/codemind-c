6bea14875bc7d9db73bf98ce2d753d177013163f
nagapallalaraju <99000421+nprloves@users.noreply.github.com>
2022-02-27 18:53:00 +0530

    #include<stdio.h> int main() {    int n;    scanf("%d",&n);
       for(int r=1; r<=n; r++)
       {
         for(int c=1; c<=n; c++)
         {
           printf("%d ",r);
         }
         printf("
    ");
       }
    
       return 0;
    }

---
+++ Pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   scanf("%d",&n);
+
+   for(int r=1; r<=n; r++)
+   {
+     for(int c=1; c<=n; c++)
+     {
+       printf("%d ",r);
+     }
+     printf("
+");
+   }
+
+   return 0;
+}
